9d935546339b9902aee5524e0ab395d23c85eb74
TatyanaKonop <115359442+TatyanaKonop@users.noreply.github.com>
2024-02-14 14:45:20 +0100

    Update test_buy_product.py

--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -12,7 +12,7 @@ class TestBuyProduct(BaseTest):
     def test_buy_product(self):
         self.pages['market_page'].add_to_cart()
         self.pages['chart_page'].go_to_checkout()
-        self.pages['checkout_page'].checkout(444)
+        self.pages['checkout_page'].checkout()
         self.pages['checkout_overview_page'].finish_purchase()
         txt_message_success = self.json_reader.read_from_json()['complete-page_data']['message_success_purchase']
         self.pages['complete_page'].assertions.have_text(Compete.FINAL_TEXT, txt_message_success, "no")
